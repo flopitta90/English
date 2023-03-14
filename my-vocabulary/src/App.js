a4192380c0080f5c56ae5c1c7a7fa78920261e96
flopitt90 <flopitta_90@hotmail.com>
2023-03-14 10:26:48 +0000

    new layout

--- my-vocabulary/src/App.js
+++ my-vocabulary/src/App.js
@@ -20,13 +20,15 @@ while(!text.text){
   )
 }
 const indexWord = text?.text?.indexOf('Meaning')
-const indexMeaning = text?.text?.indexOf('1')
+const indexMeaning = text?.text?.indexOf('Sentences:')
+const indexSentences = text?.text?.indexOf('1')
 const indexSentence1 = text?.text?.indexOf('2')
 const indexSentence2 = text?.text?.indexOf('3')
 
 const word = text?.text?.slice(7,indexWord)
 const meaning = text?.text?.slice(indexWord,indexMeaning)
-const sentence1 = text?.text?.slice(indexMeaning,indexSentence1)
+const sentences = text?.text?.slice(indexMeaning,indexSentences)
+const sentence1 = text?.text?.slice(indexSentences,indexSentence1)
 const sentence2 = text?.text?.slice(indexSentence1,indexSentence2)
 const sentence3 = text?.text?.slice(indexSentence2)
   return (
@@ -37,6 +39,7 @@ const sentence3 = text?.text?.slice(indexSentence2)
       <div className='bg-[#f1f1f1] rounded-xl p-10 drop-shadow-xl w-[95%] md:w-[50%]'>
         <h4 className='text-2xl font-bold flex align-center justify-center align-center'>{word}</h4>
         <h4 className='text-2xl font-light flex align-center justify-center'>{meaning}</h4>
+        <h4 className='text-2xl font-light flex align-center justify-center'>{sentences}</h4>
         <h4 className='text-xl font-light'>{sentence1}</h4>
         <h4 className='text-xl font-light'>{sentence2}</h4>
         <h4 className='text-xl font-light'>{sentence3}</h4>
