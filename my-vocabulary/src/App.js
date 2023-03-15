515bd993e195e26e806d6cdff55ba50f68b023bc
flopitt90 <flopitta_90@hotmail.com>
2023-03-15 13:02:40 +0000

    refactoring code

--- my-vocabulary/src/App.js
+++ my-vocabulary/src/App.js
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react';
 import logo from '../src/images/logowordup.png'
-import logoGif from '../src/images/logoturning.gif'
-import {FacebookShareButton,LinkedinShareButton,TwitterShareButton,WhatsappShareButton,TwitterIcon, LinkedinIcon, FacebookIcon, WhatsappIcon} from 'react-share'
+import { ShareButtons } from './components/ShareButtons';
 import { WordCard } from './components/WordCard';
 
 function App() {
  
   return (
-    <div className='bg-gradient-to-l from-cyan-400 to-indigo-600 min-h-screen p-5 flex flex-col items-center justify-center md:flex-row '>
-     <div>
-        <img className='h-72 w-72'src={logo}></img>
+      <div className='bg-gradient-to-l from-cyan-400 to-indigo-600 min-h-screen p-5 flex flex-col items-center justify-center md:flex-row '>
+       <div>
+          <img className='h-72 w-72'src={logo}></img>
+        </div>
+       <WordCard/>
+       <ShareButtons/>
       </div>
-     <WordCard/>
-    </div>
   );
 }
 
---
+++ my-vocabulary/src/components/ShareButtons.jsx
@@ -0,0 +1,27 @@
+import {FacebookShareButton,LinkedinShareButton,TwitterShareButton,WhatsappShareButton,TwitterIcon, LinkedinIcon, FacebookIcon, WhatsappIcon} from 'react-share'
+
+export const ShareButtons = () => {
+  return (
+    <div className='m-3 w-56 flex justify-around md:flex-col md:h-56 md: w-auto '>
+    <FacebookShareButton url='http://wordup-ai.vercel.app' quote="I didn't have a clue about this one" hashtag='#learning'>
+    <FacebookIcon borderRadius={20} size="50"/>
+    </FacebookShareButton>
+    <TwitterShareButton url='http://wordup-ai.vercel.app' 
+    title="I didn't have a clue about this one"
+    hashtags={['learning', 'wordup', 'english']}
+    related={['@florencia.confo']}
+    >
+    <TwitterIcon borderRadius={20} size="50"/>
+    </TwitterShareButton>
+    <LinkedinShareButton url='http://wordup-ai.vercel.app' 
+    title={"WordUp"}
+    summary={'WordUp its an app to learn new English words everyday, check it out!'}
+    source='http://wordup-ai.vercel.app'>
+    <LinkedinIcon borderRadius={20} size="50"/>
+    </LinkedinShareButton>
+    <WhatsappShareButton url='http://wordup-ai.vercel.app' title="I didn't have a clue about this one">
+    <WhatsappIcon borderRadius={20} size="50"/>
+    </WhatsappShareButton>
+    </div>
+  )
+}
