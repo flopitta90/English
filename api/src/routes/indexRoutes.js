beebdcc605b44df996e3c8e4624754be062d3a5b
flopitt90 <flopitta_90@hotmail.com>
2023-03-27 11:32:17 +0100

    router added 6

--- api/src/routes/indexRoutes.js
+++ api/src/routes/indexRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import * as authRouter from '../routes/authRouter'
+// import * as authRouter from '../routes/authRouter'
 import * as wordRouter from '../routes/wordRouter'
 
 export const router = Router();
 
 router.use('/word', wordRouter)
-router.use('/user', authRouter)
+// router.use('/user', authRouter)
 
