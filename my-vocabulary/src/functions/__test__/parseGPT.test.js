9dd3f1cfc00614d14acdb721a9cde0ad5536a6df
Florencia Conforti <flopitta_90@hotmail.com>
2023-04-21 18:53:33 +0100

    Update parseGPT.test.js

--- my-vocabulary/src/functions/__test__/parseGPT.test.js
+++ my-vocabulary/src/functions/__test__/parseGPT.test.js
@@ -4,7 +4,7 @@ import { parseGPTResponse } from "../parseGPT";
 describe('Parse the information coming from GPT API',()=>{
   test('should parse the info correctly', ()=>{
    const text = {text: "\n\nWord: Enmity \nMeaning: Hostility or ill will\nSentences: 1: There was a deep enmity between the two nations. 2: The feud between the two families was born out of enmity. 3: The enmity between the two companies had lasted for years."}
-   const {word,meaning,sentence1,sentence2,sentence3} = parseGPT(text)
+   const {word,meaning,sentence1,sentence2,sentence3} = parseGPTResponse(text)
    expect(word).toEqual(' Enmity \n')
    expect(meaning).toEqual('Meaning: Hostility or ill will\n')
    expect(sentence1).toEqual('1: There was a deep enmity between the two nations. ')
@@ -14,13 +14,13 @@ describe('Parse the information coming from GPT API',()=>{
 
   test('should parse the word correctly,even without the meaning', ()=>{
     const text = {text: "\n\nWord: Enmity \n"}
-    const {word} = parseGPT(text)
+    const {word} = parseGPTResponse(text)
     expect(word).toMatch(' Enmity ')
    })
 
    test('should not parse the info if text incomplete', ()=>{
     const text = {text: "\n\nWord: Enmity \nMeaning: Hostility or ill will\nSentences: "}
-    const {word,meaning,sentence1,sentence2,sentence3} = parseGPT(text)
+    const {word,meaning,sentence1,sentence2,sentence3} = parseGPTResponse(text)
     expect(word).toEqual(' Enmity \n')
     expect(meaning).toEqual('Meaning: Hostility or ill will\n')
     expect(sentence1).toBeFalsy()
